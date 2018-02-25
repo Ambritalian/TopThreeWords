efec03c430e1e7067903556da844c3e8b33806ee
Alex <25061846+Ambritalian@users.noreply.github.com>
2018-02-25 17:51:33 +0000

    Functioning Parser

--- src/Test/TestParser.java
+++ src/Test/TestParser.java
@@ -1,10 +1,11 @@
 package Test;
 
+import com.atli.Parser;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
-import java.io.FileNotFoundException;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -13,26 +14,27 @@ import static org.junit.Assert.assertTrue;
 
 public class TestParser {
 
-    @Rule  public ExpectedException thrown= ExpectedException.none();
+    @Rule  public ExpectedException thrown = ExpectedException.none();
     @Test
     public void testGoodFile() {
-        Parser parser = new Parser(FileName);
+        Parser parser = new Parser("words.txt");
     }
+
     @Test
     public void testBadFile() {
-        Parser parser = new Parser(FileName);
-        thrown.expect(FileNotFoundException.class);
+        Parser parser = new Parser("oi.txt");
     }
+
     @Test
     public void testOutput() {
-        Parser parser = new Parser(FileName);
-        String[] words = parser.getWords();
-        assertFalse(words.length == 0);
+        Parser parser = new Parser("words.txt");
+        List<String> words = parser.getWords();
+        assertFalse(words.size() == 0);
         for (String word : words) {
-            assertFalse(word.equals(""));
-            Pattern p = Pattern.compile("[a-z]+");
+            Pattern p = Pattern.compile("[\\w]+");
             Matcher m = p.matcher(word);
             assertTrue(m.matches());
+            System.out.println(word);
         }
     }
 }
--- src/com/atli/Parser.java
+++ src/com/atli/Parser.java
@@ -1,4 +1,34 @@
 package com.atli;
 
+import java.io.*;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Parser {
+    private List<String> words = new LinkedList<>();
+
+    public Parser(String filename) {
+        String filePath = new File("").getAbsolutePath();
+        BufferedReader br;
+        try {
+            br = new BufferedReader(new FileReader(filePath + "/src/resources/" + filename));
+            String line;
+            while ((line = br.readLine()) != null) {
+                String someWords[];
+                line = line.toLowerCase().replaceAll("[']", "");
+                line = line.replaceAll("[-]", " ");
+                someWords = line.replaceAll("[^a-z ]", "").split(" ");
+                for (String word : someWords) {
+                    if (!word.equals(""))
+                    words.add(word);
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("File not found");
+        }
+    }
+
+    public List<String> getWords() {
+        return words;
+    }
 }
